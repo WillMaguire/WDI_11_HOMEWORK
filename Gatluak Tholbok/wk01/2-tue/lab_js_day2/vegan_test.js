4a12a0dd1307a1a42b4fe9cb8d842389432e797b
g.tholbok@gmail.com <g.tholbok@gmail.com>
2017-02-08 08:37:11 +1100

    direct student and vegan test

---
+++ Gatluak Tholbok/wk01/2-tue/lab_js_day2/vegan_test.js
@@ -0,0 +1,17 @@
+var menuChoice = prompt("Please choose a menu");
+var onMenu = ['steak', 'pork chops', 'tofurkey', 'fruit salad'];
+if (menuChoice === 'tofurkey') {
+  console.log('This cuisine is vegan friendly');
+}
+else if (menuChoice === 'fruit salad') {
+  console.log('This cuisine vegan friendly');
+}
+if (menuChoice = 'steak') {
+console.log('Vegans beware');
+}
+else if (menuChoice = 'pork chops') {
+console.log('Vegans beware');
+}
+else {
+console.log('Sorry, this menu dish is not on the menu the tonight');
+}
