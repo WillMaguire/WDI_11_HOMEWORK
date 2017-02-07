bd02e31b53f91b29650a669a4b653dc7756875c1
Sandesh Karki <de.sandesh@gmail.com>
2017-02-07 18:39:18 +1100

    final updated

--- Sandesh Karki/wk01/2-tue/index.js
+++ Sandesh Karki/wk01/2-tue/index.js
@@ -1,8 +1,14 @@
+
+// Lab: Grabbing input form user in the broswer
+
 var name= prompt("Please enter your name ");
 console.log("Welcome, " + name + "!!");
 
 var menu= ["Fruit Salad", "Tofurkey", "Pork Chops"];
 var dinner= prompt ("What would you like for dinner tonite??\n " + menu);
+
+
+// Part 2 - The Vegan Test
 
 if(dinner === menu[1]){
   console.log("This cuisine is vegan friendly.")
@@ -10,4 +16,47 @@ if(dinner === menu[1]){
   console.log ("Vegan beware!!")
 }else{
   console.log ("Sorry, it's not in menu")
+}
+
+// conditionals
+
+// 1. Drinking age
+
+var age= prompt("Please enter your age");
+var ageInNum= Number(age);
+
+if(ageInNum < 18){
+  console.log("You are underage");
+}else{
+  console.log("You are allowed to drink");
+}
+
+// 2. Air Conditioning
+
+var acFunctional= prompt("Is it Air Conditioning functional(y/n)");
+
+var currentTemp= prompt("Please enter current Temperature");
+var desiredTemp= 18;
+
+if(acFunctional === 'y'){
+  if(currentTemp > desiredTemp){
+    console.log("Turn on the Air Conditioning Please");
+  }
+}
+else if(currentTemp > desiredTemp){
+    console.log("Fix the Air Conditioning now! It's hot!");
+}
+else if(currentTemp < desiredTemp){
+  console.log("Fix the Air Conditioning whenever you have the chance... It's cool...")
+}
+
+// Melbourne Suburbs
+
+var suburbs= prompt("What Suburb you live in ");
+var iLiveIn= "Glen Huntly";
+
+if(suburbs === iLiveIn){
+  console.log("Great, we live in same suburb");
+}else{
+  console.log("Sad we dont live in same area");
 }
