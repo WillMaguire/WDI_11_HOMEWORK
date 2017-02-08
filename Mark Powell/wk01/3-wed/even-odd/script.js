5c1d4e49046979022facb9bbb2258a7d0dd12e92
Mark Powell <hi@powellmark.com>
2017-02-08 16:34:55 +1100

    Basic exercises done

---
+++ Mark Powell/wk01/3-wed/even-odd/script.js
@@ -0,0 +1,7 @@
+
+// Create a loop that will iterate from 0 to 20
+for (var i = 0; i <= 20; i++) {
+  if ( i % 2 === 0 ) {
+    console.log(i + " is even");
+  }
+}
---
+++ Mark Powell/wk01/3-wed/multiplication/script.js
@@ -0,0 +1,6 @@
+
+// Create a loop that will iterate from 0 to 20
+for (var i = 0; i <= 10; i++) {
+  var timesNine = i * 9;
+  console.log(i + " * 9 = " + timesNine);
+}
