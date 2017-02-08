bc339d87174ff20b9505a8a8cbf24977e0c88c19
Gerard Ho <gerard.ho@live.com.au>
2017-02-08 16:18:12 +1100

    Completed Week 1 Day 3 Loops

---
+++ Gerard Ho/wk01/3-wed/loops.js
@@ -0,0 +1,44 @@
+// The even/odd reporter
+//
+// Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
+//
+// Multiplication Tables
+//
+// Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
+//
+// Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
+//
+// Your top choices
+//
+// Create an array to hold your top choices (colors, presidents, whatever).
+//
+// For each choice, log to the screen a string like: "My #1 choice is blue."
+//
+// Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+
+//The even/odd reporter
+console.log("The even/odd reporter");
+for(var i=0; i<=20; i++){
+  if(i%2===0){
+    console.log(i + " is an even number.");
+  }else{
+    console.log(i + " is an odd number.")
+  }
+}
+
+console.log("Multiplication Tables");
+for(var num=1; num<=10; num++){
+  for(var i=0; i<=10; i++){
+    multiplier = num;
+    console.log(i*num);
+  }
+}
+
+console.log("Your top choices");
+var topChoices = ["Navy Blue", "Lamborghini", "Paradise", "Lifestyle", "Health", "Financial Security", "Happiness", "Motivation", "Self Confidence"];
+
+for(var i=0; i<topChoices.length; i++){
+  var comment = "My #"+ i + " choice is "+topChoices[i];
+
+  console.log(comment);
+}
