d1b10d20bd796013b054e89b352591dc4514de45
Marta Scaramella <marta.scaramella@gmail.com>
2017-02-08 17:17:47 +1100

    homework day 3

---
+++ Marta Scaramella/wk01/3-wed/objects/recipe.js
@@ -0,0 +1,16 @@
+var recipeCard = {
+  title: 'pizza',
+  serving: 1,
+  ingredients: ['pizza dough','tomato sauce', 'mozzarella', 'basil']
+};
+
+  console.log(recipeCard.title);
+  console.log(recipeCard.serving);
+  console.log(recipeCard.ingredients[0]);
+  console.log(recipeCard.ingredients[1]);
+  console.log(recipeCard.ingredients[2]);
+
+
+// for (var index = 0; index < 3; index++) {
+//   console.log(recipeCard[index]);
+// }
