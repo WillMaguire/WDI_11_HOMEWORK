1529988314f462715495de5f25d44a0827bf817d
Mark Powell <hi@powellmark.com>
2017-02-09 10:47:21 +1100

    Added console logs for uneven numbers and for 0

--- Mark Powell/wk01/3-wed/even-odd/script.js
+++ Mark Powell/wk01/3-wed/even-odd/script.js
@@ -1,7 +1,11 @@
 
 // Create a loop that will iterate from 0 to 20
 for (var i = 0; i <= 20; i++) {
-  if ( i % 2 === 0 ) {
+  if (i === 0 ) {
+    console.log(i + ' is not dividable');
+  } else if (i % 2 === 0 ) {
     console.log(i + " is even");
+  } else {
+    console.log(i + " is not even");
   }
 }
