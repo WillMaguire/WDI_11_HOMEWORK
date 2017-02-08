364c8515322b73842605f4143aa1b8a68d0e1ebd
Mark Powell <hi@powellmark.com>
2017-02-08 21:37:39 +1100

    added multiplication table

--- Mark Powell/wk01/3-wed/multiplication/script.js
+++ Mark Powell/wk01/3-wed/multiplication/script.js
@@ -4,3 +4,12 @@ for (var i = 0; i <= 10; i++) {
   var timesNine = i * 9;
   console.log(i + " * 9 = " + timesNine);
 }
+
+var outerArray = [];
+
+for(var i = 1; i < 10; i++){
+  for(var y = 1; y < 10; y++){
+      var multiplied = y * i;
+      console.log(i + " x " + y + " + " + "= " + multiplied );
+  }
+}
