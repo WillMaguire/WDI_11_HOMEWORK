ef7bb515516c955c7edd0c6b7cb1b2b94ecc57e9
Barry Tabios <b_tabios@yahoo.com>
2017-02-08 17:41:52 +1100

    updated multiplication.html

--- Barry Tabios/wk01/3-wed/multiplication.js
+++ Barry Tabios/wk01/3-wed/multiplication.js
@@ -1,9 +1,18 @@
 console.log('multiplication JS WORKIG');
 var num;
 var multiply;
+var answer;
 
 for (var i = 0; i < 10; i++){
   num = i;
   multiply = num * 9;
   console.log(num + " * " + "9" + " = " + multiply );
+};
+
+
+for (var i = 1; i <= 10; i ++){
+  for( var k = 1; k <=10; k++){
+    answer = i * k;
+    console.log(i + " * " + k + " = " + answer);
+  };
 };
