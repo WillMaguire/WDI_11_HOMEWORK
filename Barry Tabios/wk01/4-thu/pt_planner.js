403a7a5671f28c45672fd00718ee3a21047a639b
Barry Tabios <b_tabios@yahoo.com>
2017-02-09 23:21:41 +1100

    pt_planner update

--- Barry Tabios/wk01/4-thu/pt_planner.js
+++ Barry Tabios/wk01/4-thu/pt_planner.js
@@ -7,8 +7,8 @@ var stations = {
   sandringham: ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor']
 };
 
-var input_origin = 'Glenferrie';//prompt('please enter your origin');
-var input_destination = 'Flinders Street';//prompt('please enter your desired destination');
+var input_origin = 'East Richmond';//prompt('please enter your origin');
+var input_destination = 'Glenferrie';//prompt('please enter your desired destination');
 
 var num_of_station_lines = Object.keys(stations).length; // calculates number of stations lines
 var station_length_alamein = stations.alamein.length; // calculates number of statations for alamein
@@ -17,6 +17,12 @@ var station_length_sandringham = stations.sandringham.length; // calculates numb
 
 var origin_index;
 var destination_index;
+
+var origin_key_index;
+var destination_key_index;
+var origin_value_index;
+var destination_value_index;
+
 var origin;
 var destination;
 var trip = [];
@@ -24,16 +30,21 @@ var trip = [];
 
 var changeover = 'Richmond'; // anchor point - changeover
 
+
 for (i=0; i<num_of_station_lines; i++){
   for(j=0; j<stations.alamein.length; j++){
     if(stations.alamein[j] === input_origin){ // checks alamein station
       origin_index = j; // index value the origin
-      console.log('alamein line origin:' + origin)
+      origin_key_index = 0;
+      origin_value_index = j;
+      console.log('alamein line origin:' + input_origin) // test console
     };
     if(stations.alamein[j] === input_destination){
       //call function for this
       destination_index = j;  // assigns index value of destination
-      console.log('alemein line destination:' + input_destination);
+      destination_key_index = 0;
+      destination_value_index = j;
+      console.log('alemein line destination:' + input_destination); // test console
     };
   };
 
@@ -41,11 +52,15 @@ for (i=0; i<num_of_station_lines; i++){
     if(stations.glenWaverley[k] === input_origin){ // checks glen waverley station
       //line = 'glen waverley'; // assigns the string glen waverley to line variable
       origin_index = k; // assigns the index value of origin
-      console.log('glen waverley line origin:' + origin)
+      origin_key_index = 1;
+      origin_value_index = k;
+      console.log('glen waverley line origin:' + input_origin) // test console
     };
     if(stations.glenWaverley[k] === input_destination){
       destination_index = k; // assigns the index value of destination
-      console.log('glen waverley line destination:' + input_destination);
+      destination_key_index = 1;
+      destination_value_index = k;
+      console.log('glen waverley line destination:' + input_destination); // test console
       // call function
     };
   };
@@ -54,17 +69,25 @@ for (i=0; i<num_of_station_lines; i++){
     if(stations.sandringham[l] === input_origin){ // checks glen waverley station
       //line = 'sandringham'; // assigns the string sandringham to variable line.
       origin_index = l; // assigns the index value to origin
-      console.log('sandringham waverley line origin:' + origin)
+      origin_key_index = 2;
+      origin_value_index = l;
+      console.log('sandringham waverley line origin:' + input_origin) // test console
     };
     if(stations.sandringham[l] === input_destination){
       destination_index = l; // assigns the index value to destination
-      console.log('sandringham line destination:' + input_destination);
+      destination_key_index = 2;
+      destination_value_index = l;
+      console.log('sandringham line destination:' + input_destination); // test console
       // call function;
     };
   };
 
-
 };
+
+console.log('destination key index:' + destination_key_index + ' ' + "destinatination value index:" + destination_value_index); // test console
+console.log('origin key index:' + origin_key_index + ' ' + "origin value index:" + origin_value_index); // test console
+
+
 
 if(origin_index < destination_index){ // if the origin is lower than the station
   while(origin_index <= destination_index){
