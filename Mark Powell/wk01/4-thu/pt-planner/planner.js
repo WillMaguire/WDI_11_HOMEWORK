0e299f9571b794ae9d3e82990c25bf2b9f0239be
Mark Powell <hi@powellmark.com>
2017-02-09 20:30:55 +1100

    First pass at PT planner exercise - Not finished

---
+++ Mark Powell/wk01/4-thu/pt-planner/planner.js
@@ -0,0 +1,26 @@
+// console.log("train's leaving..!");
+var trainlines = [
+  // Only using capitalized keys because they are names!
+  ['Flagstaff','Melbourne Central', 'Parliament','Richmond','Kooyong','Tooronga'],
+  ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor'],
+  ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie']
+];
+
+// Find stations between start and richmond
+for (var i = 0; i < trainlines.length; i++) {
+
+  if (trainlines[i].indexOf('Parliament') !== -1 ) {
+    // find start stations index in the array - count it against the Richmond in that array
+    // return the stations in between the start and Richmond
+  }
+}
+
+for (var i = 0; i < trainlines.length; i++) {
+  if (trainlines[i].indexOf('Burnley') !== -1 ) {
+  
+    console.log("Array containing Burnley: " + i); // index of Array that contains Burnley
+    console.log("Index of Burnley in that array: " + trainlines[i].indexOf('Burnley')); // Index
+    // find start stations index in the array - count it against the Richmond in that array
+    // return the stations in between the start and Richmond
+  }
+}
---
+++ Mark Powell/wk01/4-thu/pt-planner/pseudo-code.md
@@ -0,0 +1,8 @@
+Take input from user
+  - start station
+  - destination station
+
+Create array for each train line
+
+loop each array to see if input value is present
+loop each array to see if
