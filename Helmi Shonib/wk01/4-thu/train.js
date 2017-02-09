875aeba93d3466b0a5ebf7613cfd2d789d349d7b
full slack <helminems@hotmail.com>
2017-02-10 07:45:59 +1100

    Train Homework

---
+++ Helmi Shonib/wk01/4-thu/train.js
@@ -0,0 +1,4 @@
+var userOrigin = prompt("Please type your origin");
+var userDestination = prompt("Please type your destination");
+
+var alamein = ["Flinders St", "Richmond", "East Richmond", "Burnley", "Hawthorne", "Glenferrie"];
