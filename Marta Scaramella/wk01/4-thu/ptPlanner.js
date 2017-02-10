4ca6e20fd5994ebd7d6dedd3188ed74b4e113c28
Marta Scaramella <marta.scaramella@gmail.com>
2017-02-10 21:03:15 +1100

    correct return

--- Marta Scaramella/wk01/4-thu/ptPlanner.js
+++ Marta Scaramella/wk01/4-thu/ptPlanner.js
@@ -4,8 +4,6 @@ var ptNetwork = {
   'Sandringham Line': ['Southern Cross', 'Richmond', 'South Yarra' ,'b','Prahran', 'Windsor']
 }
 
-var node = 'Richmond';
-
 //def origin
 var origin = prompt("What's the origin of your trip?");
 if (ptNetwork['Alamein Line'].indexOf(origin) < 0 && ptNetwork['GlenWaverly Line'].indexOf(origin) < 0 && ptNetwork['Sandringham Line'].indexOf(origin) < 0) {
@@ -16,113 +14,64 @@ var destination = prompt("What's the destination of your trip?");
 if (ptNetwork['Alamein Line'].indexOf(destination) < 0 && ptNetwork['GlenWaverly Line'].indexOf(destination) < 0 && ptNetwork['Sandringham Line'].indexOf(destination) < 0) {
   destination = prompt("Try again! " + destination + " isn't a station.");
 }
-
 // origin line
-  for (var i = 0; i < Object.keys(ptNetwork).length; i++) {
-    for (var k = 0; k < Object.values(ptNetwork)[i].length; k++) {
-      if (Object.values(ptNetwork)[i][k] === origin) {
-        // var originLine = Object.keys(ptNetwork)[i];
-        var originLineIndex = i;
-        var originLineObjectIndex = k;
-        // console.log(originLine);
-        // console.log(originLineIndex);
-        // console.log('originLineObjectIndex is ' + originLineObjectIndex);
-      // }
-        if (Object.values(ptNetwork)[originLineIndex][k] === node) {
-          var originIntersectionNodeIndex = k;
-        // console.log('originIntersectionNodeIndex is ' + originIntersectionNodeIndex);
-        }
-      }
+for (var i = 0; i < Object.keys(ptNetwork).length; i++) {
+  for (var k = 0; k < Object.values(ptNetwork)[i].length; k++) {
+    if (Object.values(ptNetwork)[i][k] === origin) {
+      var originLineIndex = i;
+      var originLineObjectIndex = k;
+      var originIntersectionNodeIndex = Object.values(ptNetwork)[originLineIndex].indexOf('Richmond');
     }
-
-
-// destination line - origin - richmond
-// if (originLineObjectIndex <= originIntersectionNodeIndex+1){
-    for (var ii = 0; ii < Object.keys(ptNetwork).length; ii++) {
-      for (var kk = 0; kk < Object.values(ptNetwork)[ii].length; kk++) {
-        if (Object.values(ptNetwork)[ii][kk] === destination) {
-          // var destinationLine = Object.keys(ptNetwork)[ii];
-          var destinationLineIndex = ii;
-          var destinationLineObjectIndex = kk;
-          // console.log(destinationLine);
-          // console.log(destinationLineIndex);
-          // console.log(destinationObjectIndex);
-        // }
-        if (Object.values(ptNetwork)[ii][kk] === node) {
-          var destinationIntersectionNodeIndex = kk;
-          // console.log(destinationIntersectionNodeIndex);
-        }
-        }
+  }
+}
+// destination line
+for (var ii = 0; ii < Object.keys(ptNetwork).length; ii++) {
+  for (var kk = 0; kk < Object.values(ptNetwork)[ii].length; kk++) {
+    if (Object.values(ptNetwork)[ii][kk] === destination) {
+      var destinationLineIndex = ii;
+      var destinationLineObjectIndex = kk;
+      var destinationIntersectionNodeIndex = Object.values(ptNetwork)[ii].indexOf('Richmond');
       }
     }
   }
-// else {
-//   for (var ii = Object.keys(ptNetwork).length; ii < originIntersectionNodeIndex; ii++) {
-//     for (var kk = 0; kk < Object.values(ptNetwork)[ii].length; kk++) {
-//       if (Object.values(ptNetwork)[ii][kk] === destination) {
-//         var destinationLine = Object.keys(ptNetwork)[ii];
-//         var destinationLineIndex = ii;
-//         var destinationObjectIndex = kk;
-//         // console.log(destinationLine);
-//         // console.log(destinationLineIndex);
-//         // console.log(destinationObjectIndex);
-//       }
-//       if (Object.values(ptNetwork)[ii][kk] === node) {
-//         var destinationIntersectionNodeIndex = kk;
-//         // console.log(destinationIntersectionNodeIndex);
-//       }
-//   }
-// }
-//
-// }
-// origin richmond destination
-var trip1 = [''];
-var trip2 = [''];
-var totalTrip = [''];
 
+var trip1 = [];
+var trip2 = [];
+// from origin to Richmond
 if (originLineObjectIndex <= originIntersectionNodeIndex){
   for (var x = originLineObjectIndex; x <= originIntersectionNodeIndex; x++) {
     trip1.push(Object.values(ptNetwork)[originLineIndex][x]);
+    trip1.push(" --> ");
+    var trip1Distance = originIntersectionNodeIndex - originLineObjectIndex;
   }
-  console.log(trip1);
 }
 else {
-  for (var x = originIntersectionNodeIndex; x <= originLineObjectIndex; x++) {
+  for (var x = originLineObjectIndex; x >= originIntersectionNodeIndex; x--) {
     trip1.push(Object.values(ptNetwork)[originLineIndex][x]);
+    trip1.push(" --> ");
+    var trip1Distance = originLineObjectIndex - originIntersectionNodeIndex;
   }
-  console.log(trip1);
 }
-
-
-// if richmond is after destination
+// from Richmond to destination
 if (destinationLineObjectIndex >= destinationIntersectionNodeIndex){
-  for (var x = destinationIntersectionNodeIndex; x <= destinationLineObjectIndex; x++) {
+  for (var x = destinationIntersectionNodeIndex+1; x <= destinationLineObjectIndex; x++) {
     trip2.push(Object.values(ptNetwork)[destinationLineIndex][x]);
-    // trip2.concat([Object.values(ptNetwork)[destinationLineIndex][x]]);
-    // var stop = Object.values(ptNetwork)[destinationLineIndex][x];
-    // var stopString = stop.toString();
-    // trip2.push(stopString);
+    trip2.push(" --> ");
   }
-  // console.log(stop);
-  // console.log(stopString);
-  console.log(trip2);
+    trip2.pop();
+    var trip2Distance = destinationLineObjectIndex - destinationIntersectionNodeIndex;
 }
 else {
-    for (var x = destinationLineObjectIndex; x <= destinationIntersectionNodeIndex; x++) {
-    trip2.push(Object.values(ptNetwork)[destinationLineIndex][x]);
-    // var stop = Object.values(ptNetwork)[destinationLineIndex][x];
-    // var stopString = stop.toString();
-    // trip2.push(stopString);
-    // trip2.concat([Object.values(ptNetwork)[destinationLineIndex][x]]);
+    for (var x = destinationIntersectionNodeIndex - 1; x >= destinationLineObjectIndex; x--) {
+      trip2.push(Object.values(ptNetwork)[destinationLineIndex][x]);
+      trip2.push(" --> ");
   }
-  console.log(trip2);
+  trip2.pop();
+  var trip2Distance = destinationIntersectionNodeIndex - destinationLineObjectIndex;
 }
+// console.log(trip1);
+// console.log(trip2);
 
-var numStops = trip1.length + trip2.length;
+var totDistance = trip2Distance + trip1Distance + 1;
 
-// test
-console.log(Object.values(ptNetwork)[destinationLineIndex][1]);
-
-
-console.log( 'Origin: '+ origin + '\n' + 'Destination: '+ destination + '\n' + '\n' +
-            origin + ' '+trip1 +' '+ trip2 + ' '+destination +'\n' + numStops + ' stops total.');
+console.log( 'Origin: '+ origin + '\n' + 'Destination: '+ destination + '\n' + '\n' + trip1+ ','+ trip2 + ' '+'\n'+'\n' + totDistance + ' stops total.');
