feb643933cede1563594592d880a3e6e44e0ddd6
SamWij <sam.wijesinha@gmail.com>
2017-02-12 16:52:59 +1100

    9-02-17 Train Homework Code completed

--- Samantha Wijesinha/wk01/4-thu/train_station.js
+++ Samantha Wijesinha/wk01/4-thu/train_station.js
@@ -1,128 +1,118 @@
 console.log("train station.js connected!");
 //variables
   var intersectStation = "Richmond";
-  var originStation = "Flagstaff";
-  var destinationStation = "Tooronga";
+  var originStation = "Hawthorn";
+  var destinationStation = "Windsor";
 
 //station, key/value pairs
 
   var trainLine = [
     {
     name: 'Alamein',
-    stations:['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie'],
+    station: ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie'],
   },
     {
     name: 'Glen Waverley',
-    stations: ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga'],
+    station: ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga'],
   },
   {
     name: 'Sandringham',
-    stations: ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor']
+    station: ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor']
   },
   ];
 
+//find the  array index number of the origin STATION
 	function indexNoOriginStation (trainLine, originStation) {
 
     for (var x = 0; x < trainLine.length; x++) {
-        for (var y = 0; y < trainLine[x].stations.length; y++) {
-            if (trainLine[x].stations[y] === originStation) {
+        for (var y = 0; y < trainLine[x].station.length; y++) {
+            if (trainLine[x].station[y] === originStation) {
               var originStationInd = y;
-
             }//if statement close
         }//inner for loop close
       }//outer for loop close
 
       return originStationInd
-  }
+  };
 
   var originStationIndex = indexNoOriginStation(trainLine, originStation);
 
+  //find the index number of the origin LINE
    function indexNoOriginLine (trainLine, originStation) {
 
      for (var x = 0; x < trainLine.length; x++) {
-         for (var y = 0; y < trainLine[x].stations.length; y++) {
-             if (trainLine[x].stations[y] === originStation) {
+         for (var y = 0; y < trainLine[x].station.length; y++) {
+             if (trainLine[x].station[y] === originStation) {
                var originStationLineInd = x;
              }//if statement close
          }//inner for loop close
        }//outer for loop close
-
        return originStationLineInd
   };
   var originLineIndex = indexNoOriginLine(trainLine, originStation);
 
+
+  //find the Origin station richmond stop
+    for (var i = 0; i < trainLine[originLineIndex].station.length; i++) {
+      if (trainLine[originLineIndex].station[i] === intersectStation) {
+          var originInstesectIndex = trainLine[originLineIndex].station.indexOf(intersectStation);
+      }
+    }
+
+  //find the index number of the DESTINATION STATION
 	function indexNoDestStation (trainLine, destinationStation) {
 
     for (var x = 0; x < trainLine.length; x++) {
-        for (var y = 0; y < trainLine[x].stations.length; y++) {
-            if (trainLine[x].stations[y] === destinationStation) {
+        for (var y = 0; y < trainLine[x].station.length; y++) {
+            if (trainLine[x].station[y] === destinationStation) {
               var destStationInd = y;
             }//if statement close
         }//inner for loop close
       }//outer for loop close
       return destStationInd
   }
-
   var destStationIndex = indexNoDestStation(trainLine, destinationStation);
 
+//find the index number of the DESTINATION LINE
 	function indexNoDestLine (trainLine, destinationStation) {
     for (var x = 0; x < trainLine.length; x++) {
-        for (var y = 0; y < trainLine[x].stations.length; y++) {
-            if (trainLine[x].stations[y] === destinationStation) {
+        for (var y = 0; y < trainLine[x].station.length; y++) {
+            if (trainLine[x].station[y] === destinationStation) {
               var destStationLineInd = x;
             }//if statement close
         }//inner for loop close
       }//outer for loop close
-
       return destStationLineInd
  };
   var destLineIndex = indexNoDestLine(trainLine, destinationStation);
 
-	function calcOriginToIntersect (originLineIndex, originStationIndex, intersectStation) {
-
-    for (var i = 0; i < trainLine[originLineIndex].stations.length; i++) {
-
-      if (trainLine[originLineIndex].stations[i] === intersectStation) {
-          var originIntersectInd = i;
-      }
+  //find the destination station richmond stop
+  for (var i = 0; i < trainLine[destLineIndex].station.length; i++) {
+    if (trainLine[destLineIndex].station[i] === intersectStation) {
+        var destIntersectIndex = trainLine[destLineIndex].station.indexOf(intersectStation);
     }
-    return originIntersectInd
   }
-  var originToRichmond = calcOriginToIntersect(originLineIndex, originStationIndex, intersectStation);
-
-	function calcDestToIntersect (destLineIndex, destStationIndex, intersectStation) {
-    for (var i = 0; i < trainLine[destLineIndex].stations.length; i++) {
-
-      if (trainLine[destLineIndex].stations[i] === intersectStation) {
-          var destIntersectInd = i;
-      }
-    }
-    return destIntersectInd
-    }
-
-  var destToRichmond = calcDestToIntersect(destLineIndex, destStationIndex, intersectStation);
-
-  function calculateStops (originToRichmond, destToRichmond) {
 
 
-    if (originToRichmond-destToRichmond < 0) {
-        var stationStops = destToRichmond-originToRichmond;
+  function calculateStops (destStationIndex, originStationIndex, originInstesectIndex, destIntersectIndex) {
+    if (originStationIndex - originInstesectIndex < 0 ) {
+      var originStops = originInstesectIndex - originStationIndex;
     } else {
-      var stationStops = originToRichmond-destToRichmond
+      var originStops = originStationIndex - originInstesectIndex;
     }
-    return stationStops
+
+    if (destStationIndex -destIntersectIndex < 0 ) {
+      var destStops  = destIntersectIndex - destStationIndex;
+    } else {
+      var destStops  = destStationIndex -destIntersectIndex;
+    }
+
+      return stops = originStops+destStops
   }
+  var stops = calculateStops(destStationIndex, originStationIndex, originInstesectIndex, destIntersectIndex, trainLine);
 
- var stops = calculateStops(originToRichmond, destToRichmond);
-
-
-	function  printInformationToConsole (trainLine, destLineIndex, destStationIndex, originLineIndex, originStationIndex, originStation, destinationStation, stops) {
-
-
-
+	function  printInformationToConsole (originStation, destinationStation, stops) {
     console.log("origin: " + originStation + "\n" + "\n" + "destination: " + destinationStation + "\n" +"\n" + stops + " stops total");
-
-
   }
 
-  printInformationToConsole(trainLine, destLineIndex, destStationIndex, originLineIndex, originStationIndex, originStation, destinationStation, stops)
+  printInformationToConsole(originStation, destinationStation, stops);
