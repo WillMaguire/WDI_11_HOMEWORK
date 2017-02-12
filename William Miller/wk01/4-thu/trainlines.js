d070251f7cc6bb664070bda84d8b25aca7acc2ef
William Miller <will.oliver.miller@gmail.com>
2017-02-12 22:43:25 +1100

    trainline incomplete

--- William Miller/wk01/4-thu/trainlines.js
+++ William Miller/wk01/4-thu/trainlines.js
@@ -1,42 +1,64 @@
 console.log('HELLOOOO');
 
-// var origin = prompt ("where did your journey start");
-// var destination = prompt ("where do you want to go");
+//Inputs
 
-// trainLines = {
-//   glenWaverly: ['Tooronga', 'Kooyong', 'Richmond', 'Parliment', 'Melbourne Central', 'Flagstaff'],
-//   sandringham: [ 'Windsor', 'Prahan', 'South Yarra', 'Richmond', 'Southern Cross'],
-//   alamein: ['Glenferri', 'Hawthorn', 'Burnley', 'East Richmond', 'Richmond', 'Flinders St']
-// }
+var origin = prompt ("where did your journey start");
+var destination = prompt ("where do you want to go");
 
-var glenWaverly = ['Tooronga', 'Kooyong', 'Richmond', 'Parliment', 'Melbourne Central', 'Flagstaff'];
-var sandringham = [ 'Windsor', 'Prahan', 'South Yarra', 'Richmond', 'Southern Cross'];
-var alamein = ['Glenferri', 'Hawthorn', 'Burnley', 'East Richmond', 'Richmond', 'Flinders St']];
+//Objects
 
-var trainTotal =
-// var trainPath = origin + stopsToRich + 'Richmond' + stopsToDestination + destination
+// Array of objects trainlines[0].name trainlines[2].stations <----to access in the array of objects
+// Array of objects with 2 key/value pairs each (name, stations)
 
-// for (var i = 0; i < alamein[]; i++) {
-//   array[i]
-// }
+var trainLines = [
+  {
+    name: 'alamein',
+    stations: ['Glenferri', 'Hawthorn', 'Burnley', 'East Richmond', 'Richmond', 'Flinders St']
+  },
+  {
+    name: 'glenWaverly',
+    stations: ['Tooronga', 'Kooyong', 'Richmond', 'Parliment', 'Melbourne Central', 'Flagstaff']
+  },
+  {
+    name: 'sandringham',
+    stations: [ 'Windsor', 'Prahan', 'South Yarra', 'Richmond', 'Southern Cross']
+  }
+]
 
-var origin = 'Flinders St';
+//CALL ORIGIN/DESTINATION STOP
 
-var destination = 'Richmond';
-
-var stops = trainLines[glenWaverly].indexOf('South Yarra');
-
-
-if (origin === 'Flinders St' && destination === 'Richmond') {
-  console.log(trainTotal.alamein.slice(""));
-};
+//alamein
+var originAlamein = trainLines[0].stations.indexOf(origin);
+var inAlameinLine = trainLines[originAlamein];
+var destinationAlamein = trainLines[0].stations.indexOf(origin);
+var outAlameinLine = trainLines[destinationAlamein];
 
 
-var trainPath = function(origin, path, destination) {
-    var origin = 'Flinders'
-    var destination = 'Richmond';
-    var path = trainLines[alamein];
-    console.log(origin + );
+//Glen Waverly
+var originGlenWaverly = trainLines[1].stations.indexOf(origin);
+var inGlenWaverlyLine = trainLines[originGlenWaverly];
+var destinationGlenWaverly = trainLines[1].stations.indexOf(origin);
+var outGlenWaverlyLine = trainLines[destinationGlenWaverly];
+
+//Sandringham
+var originSandringham = trainLines[2].stations.indexOf(origin);
+var inSandringhamLine = trainLines[originSandringham];
+var destinationSandringham = trainLines[2].stations.indexOf(origin);
+var outSandringhamLine = trainLines[destinationSandringham];
+
+var commonStop = 'Richmond';
+
+
+//Find which line is the origin
+
+if (origin === inAlameinLine) {
+  var originLine = 'alamein'
+} else if (origin === inGlenWaverlyLine) {
+  var originLine = 'glenWaverlyLine'
 }
 
-trainPath()
+for (var i = 0; i < array.length; i++) {
+  array[i]
+}
+
+//Find which line is the destination
