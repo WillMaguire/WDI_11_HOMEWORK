c8da75de35bea3868b6c837f9d429d8b2b0c703c
will <mail@wilmaguire.com>
2017-02-13 17:57:28 +1100

    1-mon/ecardly_CSS

--- Will Maguire/wk01/3-wed/choices.js
+++ Will Maguire/wk01/3-wed/choices.js
@@ -1,4 +1,4 @@
 var colours = ["red","blue","green","yellow","pink"]
-  for (var i = 1; i <= colours.length; i++) {
-    console.log("My number #" + (i) + " colour is " + colours[i - 1])
+  for (var i = 0; i <= colours.length; i++) {
+    console.log("My number #" + (i+1) + " colour is " + colours)
   }
