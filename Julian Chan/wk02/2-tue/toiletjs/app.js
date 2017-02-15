12c777f597bdd3cb17f2830a9a2e61035402dad5
Julian Chan <julian.chan@mail.com>
2017-02-15 21:33:24 +1100

    refactored some code into functions

--- Julian Chan/wk02/2-tue/toiletjs/app.js
+++ Julian Chan/wk02/2-tue/toiletjs/app.js
@@ -1,14 +1,15 @@
 console.log('please flush');
 
+var body = document.getElementsByTagName('body')[0];
 var poopBtn = document.getElementById('poop-btn');
 var flushBtn = document.getElementById('flush-btn');
 
 var poop = function() {
-  document.getElementsByTagName('body')[0].style.backgroundColor = 'darkolivegreen';
+  body.style.backgroundColor = 'darkolivegreen';
 }
 
 var flush = function() {
-  document.getElementsByTagName('body')[0].style.backgroundColor = 'mintcream';
+  body.style.backgroundColor = 'mintcream';
 }
 
 poopBtn.addEventListener('click', poop);
@@ -16,28 +17,29 @@ flushBtn.addEventListener('click', flush);
 
 // dancing cat/zombie
 var movePixel = 10;
-// var reboundPixel = 20;
 var intervalMilliseconds = 800;
-var minIntervalMilliseconds = 10;
-var intervalDelta = function() { return (intervalMilliseconds - 1200) / 8; };
 var timerId = null;
 
 var zombieWalk = function() {
+  var hasReachedWindowEdge = function() {
+    return (currentLeft + imgWidth >= innerWidth && movePixel > 0) ||
+           (currentLeft <= 0 && movePixel < 0);
+  }
+
+  var reverseDirection = function() {
+    movePixel *= -1;
+  }
+
   // dom api allows to access the dom
   var images = document.getElementsByTagName('img');
   var img = images[0];
-  // var effect = images[1];
   var imgWidth = parseInt(img.style.width);
   var currentLeft = parseInt(img.style.left);
+
   img.style.left = Math.min(currentLeft + movePixel, innerWidth - imgWidth) + 'px';
-  // if (currentLeft + imgWidth >= innerWidth) {
-  //   img.style.left = currentLeft - reboundPixel + 'px';
-  //   flicker(effect);
-  // }
-  if (currentLeft + imgWidth >= innerWidth && movePixel > 0) {
-    movePixel *= -1;
-  } else if (currentLeft <= 0 && movePixel < 0) {
-    movePixel *= -1;
+
+  if (hasReachedWindowEdge()) {
+    reverseDirection();
   }
 }
 
@@ -46,10 +48,12 @@ var walkLoop = function() {
   timerId = setTimeout(walkLoop, intervalMilliseconds);
 }
 
-// var flicker = function(element) {
-//   element.style.visibility = 'visible';
-//   setTimeout(function() { element.style.visibility = 'hidden'; }, intervalMilliseconds);
-// }
+var speedUp = function() {
+  var minIntervalMilliseconds = 10;
+  var intervalDelta = (intervalMilliseconds - 1200) / 8;
+
+  intervalMilliseconds = Math.max(minIntervalMilliseconds, intervalMilliseconds + intervalDelta);
+}
 
 // one function to start
 // one function to stop
@@ -65,5 +69,5 @@ var fasterBtn = document.getElementById('gofaster-btn');
 var stopBtn = document.getElementById('stop-btn');
 
 startBtn.addEventListener('click', function() { if (timerId === null) { walkLoop(); } });
-fasterBtn.addEventListener('click', function() { intervalMilliseconds = Math.max(minIntervalMilliseconds, intervalMilliseconds + intervalDelta()); });
+fasterBtn.addEventListener('click', function() { if (timerId !== null) { speedUp(); } });
 stopBtn.addEventListener('click', function() { clearTimeout(timerId); timerId = null; })
