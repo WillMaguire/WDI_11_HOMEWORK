185a8b287d141aa4f943049f966646110474d8ca
Sandesh Karki <de.sandesh@gmail.com>
2017-02-17 10:17:50 +1100

    CakePudding

---
+++ Sandesh Karki/wk02/5-fri/cakePudding/cakePudding.js
@@ -0,0 +1,19 @@
+console.log("cakePudding");
+
+
+for(var i = 0 ; i<= 100 ; i++){
+
+
+	if(( i % 3 === 0) && (i % 5 === 0)){
+
+		console.log ("Cake Pudding");
+
+	}else if(i % 3 === 0){
+
+		console.log("Cake");
+
+	}else if (i % 5 === 0){
+
+		console.log("Pudding");
+	}
+}
