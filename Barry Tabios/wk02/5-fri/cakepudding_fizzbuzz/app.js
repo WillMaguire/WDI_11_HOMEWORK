2a8a8ed4d640232b7bd059b8112ed899b937378d
Barry Tabios <b_tabios@yahoo.com>
2017-02-17 10:27:35 +1100

    exercise

---
+++ Barry Tabios/wk02/5-fri/cakepudding_fizzbuzz/app.js
@@ -0,0 +1,34 @@
+console.log('javascript working');
+
+var num1 = prompt('please enter number');
+
+
+
+if(num1 % 3 === 0){
+  console.log('Cake');
+};
+
+if(num1 % 5 === 0){
+  console.log('Pudding');
+};
+
+if (num1 % 5 === 0 && num % 3 === 0){
+      console.log('CakePudding');
+};
+
+
+
+for(var num = 0; num<100; num++){
+
+  console.log(num);
+
+  if(num % 3 === 0){
+    console.log('Cake');
+  };
+
+  if(num % 5 === 0){
+    console.log('Pudding');
+  };
+
+
+};
