20818062a46cd742c65e17332eec6dfd401fa61a
Barry Tabios <b_tabios@yahoo.com>
2017-02-17 10:55:56 +1100

    update

--- Barry Tabios/wk02/5-fri/cakepudding_fizzbuzz/app.js
+++ Barry Tabios/wk02/5-fri/cakepudding_fizzbuzz/app.js
@@ -17,18 +17,21 @@ if (num1 % 5 === 0 && num1 % 3 === 0){
     };
 
 
+console.log('next exercise');
 
 for(var num = 0; num<100; num++){
 
+
+  if (num % 5 === 0 && num % 3 === 0){
+        console.log('CakePudding');
+      } else {
+        if(num % 3 === 0){
+          console.log('Cake');
+        } else {
+          if(num % 5 === 0){
+          console.log('Pudding');
+          };
+        };
+  };
   console.log(num);
-
-  if((num % 3) === 0){
-    console.log('Cake');
-  };
-
-  if((num % 5) === 0){
-    console.log('Pudding');
-  };
-
-
 };
