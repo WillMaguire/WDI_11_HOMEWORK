661392307c47f42fcd0b69c1228fd39d32c1e52a
will <mail@wilmaguire.com>
2017-02-17 10:31:59 +1100

    -a

---
+++ Will Maguire/wk02/5-fri/cake-is-a-lie.js
@@ -0,0 +1,13 @@
+
+
+for (var i = 0; i <= 100; i++) {
+  console.log([i]);
+  if ([i] % 3) {
+  console.log("cake")}
+    if ([i] % 5) {
+     console.log("pudding");
+       if (([i] % 3) + ([i] % 5)) {
+       console.log("cake pudding");
+     }
+  }
+};
