b6d436cd33bbac5918b5469b7236e0958dd16462
Sandesh Karki <de.sandesh@gmail.com>
2017-02-16 16:22:32 +1100

    Fashion_CSS

---
+++ Sandesh Karki/wk02/4-thu/eBanking/eBanking.js
@@ -0,0 +1,13 @@
+console.log('ebanking');
+
+
+var saving;
+var checking;
+
+
+var depoSaving;
+var depoChecking;
+
+// graps input in saving account
+var depoSav = document.querySelector('.amount');
+var getDepoSav = depoSav.value;
