6afa570a593a2232298713e8024060d295387fcb
full slack <helminems@hotmail.com>
2017-02-17 10:27:58 +1100

    cakepudding

---
+++ Helmi Shonib/wk02/4-thu/online_bank/app.js
@@ -0,0 +1,21 @@
+var totalBalance = balance
+var sav_screen = document.querySelector(.'sav_screen');
+var addTwoAmounts = function(num1, sav_screen){
+  return num1 + num2;
+}
+
+var minusTwoAmounts = function(num1, num2){
+  return num1 - num2;
+} if (totalBalance < minusTwoAmounts) {
+  return totalBalance;
+} else {
+  return minusTwoAmounts;
+}
+
+var sav_enter_amount_screen = document.querySelector(.'sav_enter_amount_screen');
+var sav_deposit_button = document.querySelector(.'sav_deposit_button')
+
+sav_deposit_button.addEventListener('click', addTwoAmounts()) {
+  var sav_enter_amount_screen = addTwoAmounts (+sav_enter_amount_screen.value +totalBalance.value);
+  sav_screen.value = result;
+});
