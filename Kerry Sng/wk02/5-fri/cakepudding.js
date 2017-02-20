bd1f1093dd8f2cf4d5e79c966a177659aefaf6ff
Kerry <kerrysng@gmail.com>
2017-02-21 08:58:02 +1100

    homework

--- Kerry Sng/wk02/5-fri/cakepudding.js
+++ Kerry Sng/wk02/5-fri/cakepudding.js
@@ -5,6 +5,7 @@ for (var i = 1; i <= 100; i+= 1) {
       console.log("Cake");
     } else if (i % 5 === 0) {
       console.log("pudding");
-    } else
-      console.log("Cakepudding");
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log("cakepudding");
+    }
 }
