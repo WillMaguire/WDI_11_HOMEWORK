1efd8b1c6d8a4b0b34a6a634439baf92144f0208
Kevin(Yuxuan) Yang <kevinyang970@hotmail.com>
2017-02-20 15:18:26 +1100

    updated 20/02/2017

--- Kevin Yang/wk02/5-fri/main.js
+++ Kevin Yang/wk02/5-fri/main.js
@@ -3,14 +3,14 @@ var result
 
     for (var i = 1; i <= 100; i++) {
 
-        if (i % 3 === 0){
-            result = "Cake, ";
+      if (i % 3 === 0 && i % 5 === 0){
+        result = "CakePudding, ";
           } else if(i % 5 === 0){
             result = "Pudding, ";
-          } else if (i % 3 === 0 && i % 5 === 0){
-            result = "CakePudding, ";
+          } else if (i % 3 === 0 ){
+            result = "Cake, ";
           } else {
             result = i;
           }
-          console.log(result)
+          console.log(result);
       }
