128d8e3c475e0453b65376446c400119b70d997a
Marta Scaramella <marta.scaramella@gmail.com>
2017-02-27 18:38:56 +1100

    homework week04, day1

---
+++ Marta Scaramella/wk04/1-mon/arrays.rb
@@ -0,0 +1,27 @@
+days_of_the_week = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday'
+]
+
+days_of_the_week.insert(0, days_of_the_week[days_of_the_week.length-1])
+
+days_of_the_week.pop()
+
+days_of_the_week_2 = [
+  'Saturday',
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday'
+]
+
+days_of_the_week_2[2,6]
+
+new_days_of_the_week_2 = days_of_the_week_2.sort
