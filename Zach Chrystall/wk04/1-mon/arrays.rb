eb373eded4dce6f9e54e519b3b8e9245937725f8
Zach Chrystall <z.chrystall@gmail.com>
2017-02-28 16:42:14 +1100

    this ruby stuff is as hard a diamonds

--- Zach Chrystall/wk04/1-mon/arrays.rb
+++ Zach Chrystall/wk04/1-mon/arrays.rb
@@ -2,12 +2,31 @@ require 'fileutils'
 require 'pry'
 
 
-days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+# days_of_the_week = 'mon tue wed thur fri sat sun'.split
+days_of_the_week = %w(mon tue wed thur fri sat sun)
+
+puts days_of_the_week
+puts '--------------------------'
 
 days_of_the_week.unshift(days_of_the_week.pop)
 
+puts days_of_the_week
+puts '--------------------------'
+
 split_week = [days_of_the_week[1..5],[days_of_the_week[0], days_of_the_week[6]]]
+
+puts split_week
+puts '--------------------------'
+
 
 split_week.pop
 
-split_week[0].sort
+puts split_week
+puts '--------------------------'
+
+
+split_week.flatten.sort
+
+puts split_week
+puts '--------------------------'
