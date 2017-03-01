615655d4d493f00127e9ea9617920a7be3be2c14
Kerry <kerrysng@gmail.com>
2017-03-01 11:02:49 +1100

    hashes

--- Kerry Sng/wk04/1-mon/app.rb
+++ Kerry Sng/wk04/1-mon/app.rb
@@ -1,18 +1,18 @@
-# puts "enter a value between 0 and 10"
-#
-# input = gets()
-#
-# number = rand(0..10)
-#
-# while input == number
-#   puts "Congragulations, you are correct"
-#
-#   if input > number
-#     puts "Wrong! Guess lower"
-#   elsif input < number
-#     puts "Wrong! Guess higher"
-#   end
-# endS
+puts "enter a value between 0 and 10"
+
+input = gets.chomp.to_i #converts to interger
+
+number = rand(0..10)
+
+while input == number
+  puts "Congragulations, you are correct"
+
+  if input > number
+    puts "Wrong! Guess lower"
+  elsif input < number
+    puts "Wrong! Guess higher"
+  end
+
 
 days_of_the_week = ["Monday" ,"Tuesday" ,"Wednesday" ,"Thursday", "Friday", "Saturday" ,"Sunday"]
 
@@ -25,5 +25,15 @@ seperate_week = [rotated[1..5], [rotated[0], rotated[6]]]
 seperate_week.pop
 puts seperate_week
 
-seperate_week.sort_by(|word|word.downcase)
+seperate_week.flatten.sort
 puts seperate_week
+
+
+
+cohort = {
+  name: 'fullslack',
+  age: 4,
+  likes: 'css'
+}
+
+#access name => cohort[:name]
--- Kerry Sng/wk04/1-mon/calculator.rb
+++ Kerry Sng/wk04/1-mon/calculator.rb
@@ -1,64 +1,33 @@
-# puts "Type 1 to add, 2 to subtract, 3 to multipy, 4 to divide"
-#
-# operation_selection = gets()
-#
-# calculate_answer(operation_selection, a,b)
-#
-# if operation_selection == 1
-#    return "add"
-#   elsif operation_selection == 2
-#     return "divide"
-#   elsif operation_selection == 3
-#     return "multipy"
-#   elsif operation_selection == 4
-#     return "divide"
-#   else
-#     return "error"
-#   end
-#
+puts "Type 1 to add, 2 to subtract, 3 to multipy, 4 to divide"
 
-#
-# def calculate_answer(operator, a, b)
-#   if operator == "add"
-#     return result = a + b
-#   elsif operator == "subtract"
-#     return result = a - b
-#   elsif operator == "multiply"
-#     return result = a * b
-#   elsif operator == "divide"
-#     return result = a / b
-#     end
-#   end
+operation_selection = gets()
 
-puts "\n" + "Type 1 to add, 2 to subtract, 3 to multiply, or 4 to divide two numbers: "
-  operation_selection = gets.to_i
+calculate_answer(operation_selection, a,b)
 
-  if operation_selection == 1
-    return "add"
+if operation_selection == 1
+   return "add"
   elsif operation_selection == 2
-    return "subtract"
+    return "divide"
   elsif operation_selection == 3
-    return "multiply"
+    return "multipy"
   elsif operation_selection == 4
     return "divide"
   else
     return "error"
-
   end
 
 
-# This method performs the requested calculation
-# It returns the result of the calculation
+
 def calculate_answer(operator, a, b)
-
   if operator == "add"
-    return  a + b
+    return result = a + b
   elsif operator == "subtract"
-    return  a - b
+    return result = a - b
   elsif operator == "multiply"
-    return a * b
+    return result = a * b
   elsif operator == "divide"
-    return a / b
+    return result = a / b
+    end
   end
 
-end
+calculate_answer(operator, a, b)
