73f67af74b9966323014a0f6ac2c2d0a41704b84
SamWij <sam.wijesinha@gmail.com>
2017-03-01 21:06:15 +1100

    updated happitails

--- Samantha Wijesinha/wk04/3-wed/happitails/app.rb
+++ Samantha Wijesinha/wk04/3-wed/happitails/app.rb
@@ -6,31 +6,76 @@ require_relative 'client.rb'
 
 animal_array = []
 client_array = []
-
-animal_array.push(Animals.new("barney", 2, "male", "dog"))
-animal_array.push(Animals.new("Mr Whiskers", 10, "male", "cat"))
-animal_array[0].a_toys.push("ball")
-animal_array[1].a_toys.push("rubber chicken")
+request = 0
 
 
-client_array.push(Clients.new("Barry Jones", 2, 55))
-client_array[0].c_pets.push("barney","gerald")
+
+loop do request != 5
+  puts "Enter 1 to display Animals \n Enter 2 to display Clients \n Enter 3 to create an Animal \n Enter 4 to create a client \n 5 to Exit"
+  request = gets.to_i
+
+  if request == 1
+
+    animal_array.each do |(a,b)|
+      puts "Animal List: #{a} #{b}"
+    end
+    request = 0
+  elsif request == 2
+    client_array.each do |(x,y)|
+      puts "Client List: #{x} #{y}"
+    end
+    request = 0
+  elsif request == 3
+      puts "Enter Animal Name:"
+      animal_name = gets.chomp
+      puts "Enter Animal Age"
+      animal_age = gets.chomp.to_i
+      puts "Enter Animal Gender"
+      animal_gender = gets.chomp
+      puts "Enter Animal Species"
+      animal_species = gets.chomp
+      puts "Enter Animal Toys"
+      animal_toys = gets.chomp
+
+      animal_array.push(Animals.new(animal_name, animal_age, animal_gender, animal_species))
+      animal_array.last.a_toys.push(animal_toys)
+      request = 0
+  elsif request == 4
+    puts "Enter Client Name:"
+    client_name = gets.chomp
+    puts "Enter Client Number of Children"
+    client_children = gets.chomp.to_i
+    puts "Enter Client Age"
+    client_age = gets.chomp.to_i
+    puts "Enter Pets"
+    client_pets = gets.chomp
+
+    client_array.push(Clients.new(client_name, client_children, client_age))
+    client_array.last.c_pets.push(client_pets)
+
+    request = 0
 
 
-client_array.push(Clients.new("Celia Swinton", 0, 30))
-client_array[1].c_pets.push("Mr Whiskers")
+    elsif request == 4
+  break
+
+  end
+
+end
+
+
+
+
+
+
 
 
 
   # The shelter should display all the clients.
   # The shelter should display all the animals.
-animal_array.each do |(a,b)|
-  puts "Animal List: #{a} #{b}"
-end
 
-client_array.each do |(x,y)|
-  puts "Animal List: #{x} #{y}"
-end
+
+
 
 
 binding.pry
