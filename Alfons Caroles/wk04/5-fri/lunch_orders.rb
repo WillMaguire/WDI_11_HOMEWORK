37b0898ac79db5be9e7644d02c7cf1d4c0ec8743
Alfons Caroles <cllin7787@gmail.com>
2017-03-03 10:27:39 +1100

    finish lunch order

---
+++ Alfons Caroles/wk04/5-fri/lunch_orders.rb
@@ -0,0 +1,21 @@
+require "pry"
+
+def collect_orders
+  puts "#{@customer_name} wants to order:"
+  customer_order = gets.chomp.to_s
+  return customer_order
+end
+
+all_order = {}
+puts "Name for order:"
+@customer_name = gets.chomp.to_s
+all_order[:name] = @customer_name
+agregate_order = []
+customer_response = 'y'
+while customer_response == 'y' do
+  agregate_order.push(collect_orders())
+  puts "Add another item to the order? (y/n)"
+  customer_response = gets.chomp
+end
+all_order[:Order] = agregate_order
+puts "All orders: #{all_order}"
