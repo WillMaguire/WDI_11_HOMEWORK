147e9ad76581fcf02838045c1f12b10e21d35425
Alfons Caroles <cllin7787@gmail.com>
2017-03-03 14:56:05 +1100

    Add multiple customers input

--- Alfons Caroles/wk04/5-fri/lunch_orders.rb
+++ Alfons Caroles/wk04/5-fri/lunch_orders.rb
@@ -6,16 +6,27 @@ def collect_orders
   return customer_order
 end
 
-all_order = {}
-puts "Name for order:"
-@customer_name = gets.chomp.to_s
-all_order[:Name] = @customer_name
-agregate_order = []
-customer_response = 'y'
-while customer_response == 'y' do
-  agregate_order.push(collect_orders())
-  puts "Add another item to the order? (y/n)"
-  customer_response = gets.chomp
+def single_client
+  client_order = {}
+  puts "Name for order:"
+  @customer_name = gets.chomp.to_s
+  client_order[:Name] = @customer_name
+  agregate_order = []
+  customer_response = 'y'
+  while customer_response == 'y' do
+    agregate_order.push(collect_orders())
+    puts "Add another item to the order? (y/n)"
+    customer_response = gets.chomp
+  end
+  client_order[:Order] = agregate_order
+  return client_order
 end
-all_order[:Order] = agregate_order
-puts "All orders: #{all_order}"
+
+all_order = []
+usr_response = 'y'
+while usr_response == 'y'
+  all_order.push(single_client())
+  puts "Do you have another customer who like to order? (y/n)"
+  usr_response = gets.chomp
+end
+puts "All Order: #{all_order}"
