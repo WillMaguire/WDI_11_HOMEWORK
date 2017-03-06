f38038d8e8d7da5d95178263d2aa5cc32fc5d093
g.tholbok@gmail.com <g.tholbok@gmail.com>
2017-03-07 00:18:20 +1100

    Monday WK5 Homework

--- Gatluak Tholbok/wk04/2-tue/happiTails/app.rb
+++ Gatluak Tholbok/wk04/2-tue/happiTails/app.rb
@@ -38,3 +38,10 @@ animal_shelter["toys"] = gets.chomp.split
 puts animal_shelter
 puts
 puts client_shelter
+
+
+# Hey I kind of messed up with this one. Instead of using the classes like we were supposed to. I went with hashes, some how I got it mixed up. I got really confused using the Classes methods. And I was wondering if I could just ask you two things tomorrow.
+
+#I was able to prompt the user through most of the entire process and I feel as though I didnt do it entirely right. It's like only half done. I got stuck with facilitating client adopts an animal and I'm still unable to get to grasp with how to create a relationship between the client and animal.
+
+#Will you have you have five minutes just to go over just these bits with me with tomorrow? I'm kind of getting things now a little. I think staying back on friday having that hard as time gettings things through my head really helped. Thanks for that one. 
