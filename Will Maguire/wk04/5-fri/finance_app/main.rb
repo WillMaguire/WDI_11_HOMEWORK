2678a52beeffeb3cf2a627f3ca90c55cd696458a
will <mail@wilmaguire.com>
2017-03-06 22:51:26 +1100

    hw

--- Will Maguire/wk04/5-fri/finance_app/main.rb
+++ Will Maguire/wk04/5-fri/finance_app/main.rb
@@ -7,11 +7,13 @@ get '/' do
   erb :index
 end
 
-# # get '/' + result do  //How to parse in a request here properly?  I keep breaking it.  The concept makes perfect sense and I'm able to retrieve the data and understand its structure... just these damned redirects(I raged out and started over a few times)
-#   erb :index
-# end
+get '/?stock_name='
+ erb :index
+end
 
-ticker = ticker.chomp
+
+
+stock_name = ticker.chomp
 ticker = @ticker
 
 result = YahooFinance::get_quotes(YahooFinance::StandardQuote, @ticker)
