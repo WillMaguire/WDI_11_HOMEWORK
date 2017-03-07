f1f55a140904cf6277e68cde6cc5c5f5e41de613
Marta Scaramella <marta.scaramella@gmail.com>
2017-03-07 18:48:27 +1100

    db_crud exercise

--- Marta Scaramella/wk05/1-mon/movies/app.rb
+++ Marta Scaramella/wk05/1-mon/movies/app.rb
@@ -8,9 +8,22 @@ get '/' do
   erb :index
 end
 
-get '/result' do
-  address = 'http://omdbapi.com/?t=' + params['t']
-  result = HTTParty.get(address)
+get '/movie_list' do
+  result = HTTParty.get('http://omdbapi.com/?s=' + params['s'])
+  # @title = array_result['Title']
+  movies = result['Search']
+  @all_movies = []
+  movies.each do |movie|
+    @all_movies_titles << movie['Title']
+  end
+
+  erb :movie_list
+end
+
+get '/movie' do
+  # t = params['s']
+  # address = 'http://omdbapi.com/?t=' + params['t']
+  # list_result = HTTParty.get(address)
   @title = result['Title']
   @year = result['Year']
   @genre = result ['Genre']
@@ -24,3 +37,5 @@ get '/result' do
 end
 
 # binding.pry
+# HTTParty.get('http://omdbapi.com/?s=jaws')
+# HTTParty.get('http://omdbapi.com/?s=jaws')['Search'][0]['Title']
