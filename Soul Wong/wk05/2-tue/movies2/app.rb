31e9cb1a1ff21c90c0260d24118d4806d17463c6
Soul Wong <sou.wei.wong@gmail.com>
2017-03-07 19:04:11 +0800

    Movies hw with multiple movies with same heading issue

---
+++ Soul Wong/wk05/2-tue/movies2/app.rb
@@ -0,0 +1,25 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+
+get '/' do
+  if( params[:movie_name].nil? )
+    erb :index
+  else
+  @result = HTTParty.get("http://omdbapi.com/?s=#{params[:movie_name]}")
+  @num_of_results = @result["totalResults"].to_i
+    if( @num_of_results == 1)
+    @name_of_movie = @result["Search"][0]["imdbID"]
+    @movieSingle = HTTParty.get("http://omdbapi.com/?i=#{@name_of_movie}")
+    if( @movieSingle["Response"] == "True")
+      erb :about
+    else
+      "Movie not found"
+    end
+
+    else
+    erb :index
+    end
+  end
+end
