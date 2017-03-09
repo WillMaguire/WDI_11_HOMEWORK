0f8d6808f95f789b69a8aaeb6fe20113a4bc2c34
Marta Scaramella <marta.scaramella@gmail.com>
2017-03-10 10:11:30 +1100

    movie with db, no link with a single title

--- Marta Scaramella/wk04/3-wed/app.rb
+++ Marta Scaramella/wk04/3-wed/app.rb
@@ -1,63 +1,64 @@
 require 'pry'
-
 require_relative 'animals'
 require_relative 'clients'
 
-@animals = []
-@clients = []
+@@animals = []
+@@clients = []
 toys = []
 
-puts "Do you want add an animal/client or display animals/clients? (type add or display)"
+puts "Select your option: \n
+1. Display all animals,\n
+2. Display all clients,\n
+3. Create an animal,\n
+4. Create a client,\n
+5. Facilitate client adopts an animal,\n
+6. Facilitate client puts an animal up for adoption"
 
-choice = gets.chomp
+choice = gets.chomp.to_i
 
 
 # loop do
 # puts 'Enter in the number you want access'
 
-if choice == 'add'
-  puts 'do you want add an animal or a client?'
-  to_add = gets.chomp
+if choice == 1
+  @@animals
 
-  if to_add == 'animal'
-    puts "Name:"
-    a_name = gets.chomp
-    puts "Age:"
-    a_age = gets.chomp.to_i
-    puts "Gender:"
-    a_gender = gets.chomp
-    puts "Species:"
-    a_species = gets.chomp
-    puts "Toys:"
-    a_toys = gets.chomp
+elsif choice == 2
+  @@clients;
 
-    animal_array.push(Animal.new(a_name, a_age, a_gender, a_species))
+elsif choice == 3
+  puts "Name:"
+  a_name = gets.chomp
+  puts "Age:"
+  a_age = gets.chomp.to_i
+  puts "Gender:"
+  a_gender = gets.chomp
+  puts "Species:"
+  a_species = gets.chomp
+  puts "Toys:"
+  a_toys = gets.chomp
 
+  @@animals << Animal.new(a_name, a_age, a_gender, a_species, a_toys)
 
+elsif choice == 4
+  puts "Name:"
+  c_name = gets.chomp
+  puts "Number of children:"
+  c_children = gets.chomp.to_i
+  puts "Age:"
+  c_age = gets.chomp.to_i
+  puts "Pets:"
+  c_pets = gets.chomp
 
-  elsif to_add == 'client'
-    puts "Name:"
-    c_name = gets.chomp
-    puts "Number of children:"
-    c_children = gets.chomp.to_i
-    puts "Age:"
-    c_age = gets.chomp.to_i
-    puts "Pets:"
-    c_pets = gets.chomp
-  end
+  @@clients << Client.new(c_name, c_children, c_age, c_pets)
 
+elsif choice == 5
+  puts "Client name:"
+  client = gets.chomp
 
-elsif choice == 'display'
-  puts 'Do you want diplay animals or clients? \n (type a for animals or c for clients)'
-  choice2 = gets.chomp
-
-    if choice2 == 'a'
-      puts 'The animals into the shelter are:'
-
-    elsif choice2 == 'c'
-      puts 'The clients of the shelter are:'
-
-    end
+elsif choice == 6
+  puts "Client name:"
 
 end
+
 binding.pry
--- Marta Scaramella/wk05/1-mon/movies/app.rb
+++ Marta Scaramella/wk05/1-mon/movies/app.rb
@@ -1,8 +1,10 @@
 require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
+require "pg"
 require 'httparty'
-
+require_relative 'database_config'
+require_relative 'models/movie'
 
 get '/' do
   erb :index
@@ -10,27 +12,54 @@ end
 
 get '/movie_list' do
   result = HTTParty.get('http://omdbapi.com/?s=' + params['film'])
-  @movies = result['Search']
-  erb :movie_list
+  if result['Search'].count === 1
+    redirect "/movie/#{params[:film]}"
+  else
+    @movies = result['Search']
+    erb :movie_list
+  end
+
 end
 
 get '/movie/:name' do
-  result = HTTParty.get("http://omdbapi.com/?t="+"#{params[:name]}")
-  # result = HTTParty.get('http://omdbapi.com/?t=' + params['film'])
+  result = Movie.find_by(title: "#{params[:name]}")
+  if result != nil
+    @title = result.title
+    @year = result.year
+    @genre = result.genre
+    @actors = result.actors
+    @plot = result.plot
+    @country = result.country
+    @awards = result.awards
+    @director = result.director
+    @poster_url = result.poster_url
 
-  @title = result['Title']
-  @year = result['Year']
-  @genre = result ['Genre']
-  @actors = result['Actors']
-  @plot = result['Plot']
-  @country = result['Country']
-  @awards = result['Awards']
-  @director = result['Director']
-  @poster = result['Poster']
+  else
+    result_external_db = HTTParty.get("http://omdbapi.com/?t="+"#{params[:name]}")
+    @title_external_db = result_external_db['Title']
+    @year_external_db = result_external_db['Year']
+    @genre_external_db = result_external_db['Genre']
+    @actors_external_db = result_external_db['Actors']
+    @plot_external_db = result_external_db['Plot']
+    @country_external_db = result_external_db['Country']
+    @awards_external_db = result_external_db['Awards']
+    @director_external_db = result_external_db['Director']
+    @poster_url_external_db = result_external_db['Poster']
+
+    Movie.create(
+      :title => @title_external_db,
+      :year => @year_external_db,
+      :genre => @genre_external_db,
+      :actors => @actors_external_db,
+      :plot => @plot_external_db,
+      :country => @country_external_db,
+      :awards => @awards_external_db,
+      :director => @director_external_db,
+      :poster_url => @poster_url_external_db
+      )
+
+  end
+
   erb :movie
-end
 
-# binding.pry
-#TEST
-# result = HTTParty.get('http://omdbapi.com/?s=jaws');
-# movies = HTTParty.get('http://omdbapi.com/?s=jaws')['Search'];
+end
