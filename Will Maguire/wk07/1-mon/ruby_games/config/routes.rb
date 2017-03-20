ab13861fbb88f088e1cc362cb4e970e3f13c872b
will <mail@wilmaguire.com>
2017-03-20 23:28:04 +1100

    -m

--- Will Maguire/wk07/1-mon/ruby_games/config/routes.rb
+++ Will Maguire/wk07/1-mon/ruby_games/config/routes.rb
@@ -5,7 +5,9 @@ get '/games', to: 'games#index'
 get '/magic8ball', to: 'games#magic8ball'
 post 'magic8ball', to: 'games#magic8ball'
 get '/secretnumber', to: 'games#secretnumber'
-post '/secretnumber', to: 'games#secretnumber'
+post '/secretnumber', to: 'games#secret_number_answer'
 get '/rockpaperscissors', to: 'games#rockpaperscissors'
-
+post '/rockpaperscissors/:throw', to: 'games#rock_paper_scissors_play'
+get '/winner', to: 'games#winner'
+get '/looser', to: 'games#looser'
 end
