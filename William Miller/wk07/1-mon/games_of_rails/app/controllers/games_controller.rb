30582acce4af0363a30772a82fe649e53dc0a248
William Miller <will.oliver.miller@gmail.com>
2017-03-20 23:18:17 +1100

    game of rails incomplete

---
+++ William Miller/wk07/1-mon/games_of_rails/app/controllers/games_controller.rb
@@ -0,0 +1,31 @@
+class GamesController < ApplicationController
+
+  def magic_eight
+
+    @question = params[:question]
+
+    num = rand(1..6)
+
+    if num == 1
+      @answer = "Don\'t count on it"
+
+    elsif num == 2
+      @answer = "It looks doubtful"
+
+    elsif num == 3
+      @answer = "Do you even need to ask?"
+
+    elsif num == 4
+      @answer = "Things are looking up"
+
+    elsif num == 5
+      @answer = "All signs point to yes"
+
+    elsif num == 6
+      @answer = "Don't ask me Im just a stupid eight ball"
+    end
+    
+  end
+
+
+end
