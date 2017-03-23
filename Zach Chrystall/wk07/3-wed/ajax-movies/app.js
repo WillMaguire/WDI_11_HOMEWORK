d8de7a7b55003ed0294e133053b24741505ce1c3
Zach Chrystall <z.chrystall@gmail.com>
2017-03-23 11:29:45 +1100

    applied css styling and created js cards

--- Zach Chrystall/wk07/3-wed/ajax-movies/app.js
+++ Zach Chrystall/wk07/3-wed/ajax-movies/app.js
@@ -5,6 +5,8 @@ var $result = $('.result')
 
 $searchBtn.on('click', function() {
 
+  $result.empty()
+
   var $searchInput = $('input');
   var title = $searchInput.val()
 
@@ -14,12 +16,18 @@ $searchBtn.on('click', function() {
   }).done(function(list) {
     movies = list['Search']
     for (var i = 0; i < movies.length; i++) {
+      $card = $('<div>')
+      $card.addClass('ui-card')
       $link = $('<a>');
       $link.attr('href', "http://www.imdb.com/title/" + movies[i]["imdbID"] + "/?ref_=nv_sr_1");
-      $movie = $('<h2>');
-      $movie.text(movies[i]["Title"]);
-      $link.append($movie);
-      $result.append($link);
+      $poster = $('<img>')
+      $poster.attr('src', movies[i]["Poster"])
+      $title = $('<h2>');
+      $title.text(movies[i]["Title"]);
+      $link.append($poster)
+      $link.append($title);
+      $card.append($link)
+      $result.append($card);
     }
   });
 });
