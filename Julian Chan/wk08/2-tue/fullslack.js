e2611e0abf150c5c00962db7f836e3fa4bd1c675
Julian Chan <julian.chan@mail.com>
2017-03-28 23:51:41 +1100

    added wk08/2-tue

---
+++ Julian Chan/wk08/2-tue/fullslack.js
@@ -0,0 +1,69 @@
+const VERSION = '1.0';
+
+// helpers
+var randInt = function(from, until) {
+  return Math.floor(Math.random() * (until - from) + from);
+};
+
+var each = function(arr, func) {
+  for (var i = 0; i < arr.length; i++) {
+    func(arr[i]);
+  }
+};
+
+// library methods
+var shuffle = function(arr) {
+  var res = arr.slice(), endIdx = res.length, idx, temp;
+
+  while (endIdx > 0) {
+    idx = randInt(0, endIdx);
+    endIdx--;
+    temp = res[endIdx];
+    res[endIdx] = res[idx];
+    res[idx] = temp;
+  }
+
+  return res;
+};
+
+var some = function(arr, predicate) {
+  for (var i = 0; i < arr.length; i++) {
+    if (predicate(arr[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+var map = function(arr, func) {
+  var res = [];
+
+  each(arr, function(elem) {
+    res.push(func(elem));
+  });
+
+  return res;
+};
+
+var filter = function(arr, predicate) {
+  var res = [];
+
+  each(arr, function(elem) {
+    if (predicate(elem)) {
+      res.push(elem);
+    }
+  });
+
+  return res;
+};
+
+// library object
+var Fullslack = {
+  VERSION: VERSION,
+  shuffle: shuffle,
+  some: some,
+  map: map,
+  filter: filter
+}
+
+debugger
