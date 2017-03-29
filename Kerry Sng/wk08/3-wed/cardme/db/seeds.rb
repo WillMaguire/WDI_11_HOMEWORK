76e4822fcf2d724c11eee72d290e71188dbf4718
Kerry <kerrysng@gmail.com>
2017-03-30 09:47:18 +1100

    cardme

---
+++ Kerry Sng/wk08/3-wed/cardme/db/seeds.rb
@@ -0,0 +1,6 @@
+Card.destroy_all
+
+Card.create name: 'king', image_url: 'https://www.placecage.com/c/200/300'
+Card.create name: 'queen', image_url: 'https://www.placecage.com/c/200/300'
+Card.create name: 'jack', image_url: 'https://www.placecage.com/c/200/300'
+Card.create name: 'ace', image_url: 'https://www.placecage.com/c/200/300'
