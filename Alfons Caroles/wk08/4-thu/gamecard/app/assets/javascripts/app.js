4a434e7e1885c59103eda1cfbdda622026919920
Alfons Caroles <cllin7787@gmail.com>
2017-03-30 17:44:51 +1100

    finished shuffle card

---
+++ Alfons Caroles/wk08/4-thu/gamecard/app/assets/javascripts/app.js
@@ -0,0 +1,95 @@
+$(document).ready(function(){
+  var QuoteItemView = Backbone.View.extend({
+template: Handlebars.compile( $('#card-template').html() ),
+render: function(){
+    var html = this.template(this.model);
+    this.$el.html(html);
+  }
+});
+
+$.ajax({
+    url: '/api/cards'
+  }).done(function(res){
+    console.log(res);
+    var shuffleCards = _.shuffle(res);
+    shuffleCards.forEach(function(card) {
+      var view = new QuoteItemView({ model: card });
+      view.render();
+      $('.contents.wrapper').append(view.el);
+    })
+  })});
+
+// new card
+$(document).ready(function(){
+  $('.new-card-form').on('submit',function(event){
+    event.preventDefault();
+    $.ajax({
+      url: '/api/cards',
+      method: 'post',
+      data: {
+        name: $('.new-card-name').val(),
+        image_url: $('.new-card-image_url').val()
+      }
+    }).done(function(card){
+      event.preventDefault();
+      var source = $('#card-template').html(); //grab the template string
+      var template = Handlebars.compile( source ); //turn template string into a function
+      var html = template( card ); //html with template & data merged together
+      $('.contents').append(html);
+    })
+  });
+
+// delete
+    $('.wrapper').on('click', '.delete-action', function(event){
+      var id = $(event.target).closest('.card').data('id');
+
+      $.ajax({
+        url: '/api/cards/' + id,
+        method: 'delete'
+      }).done(function() {
+        $(event.target).closest('.card').fadeOut(500, function(){
+          $(this).remove();
+        });
+      });
+    });
+  });
+  
+  //edit
+  $('.wrapper').on('click', '.edit-action', function(event){
+    var id = $(event.target).closest('.card').data('id');
+    var contentsWrapper = $(event.target).closest('.contents.wrapper');
+    var newForm = contentsWrapper.closest('body').find('.new-card-form');
+    var editForm = contentsWrapper.closest('body').find('.edit-card-form');
+    contentsWrapper.html('');
+    newForm.css('display', 'none');
+    editForm.css('display', 'block');
+
+  $('.edit-card-form').on('submit',function(event){
+    event.preventDefault();
+    var editId = id;
+    $('.edit-card-form').css('display','none');
+    $('.new-card-form').css('display', 'block');
+    $.ajax({
+      url: '/api/cards/' + id + '/edit',
+      method: 'get',
+      data: {
+        name: $('.edit-card-name').val(),
+        image_url: $('.edit-card-image_url').val()
+      }
+    }).done(function(card){
+      $.ajax({
+        url: '/api/cards'
+      }).done(function(res){
+        var shuffleCards = _.shuffle(res);
+
+        shuffleCards.forEach(function(card) {
+
+          var source = $('#card-template').html(); //grab the template string
+          var template = Handlebars.compile( source ); //turn template string into a function
+          var html = template( card ); //html with template & data merged together
+          $('.contents').append(html);
+        })
+    })
+  });
+});
+});
---
+++ Alfons Caroles/wk08/4-thu/gamecard/db/seeds.rb
@@ -0,0 +1,6 @@
+Card.destroy_all
+
+Card.create name: 'King', image_url: 'https://www.placecage.com/c/400/500'
+Card.create name: 'Queen', image_url: 'https://s-media-cache-ak0.pinimg.com/474x/54/6f/bd/546fbd7a4994b5e76d22a1b523fa2977.jpg'
+Card.create name: 'Jack', image_url: 'https://us.123rf.com/450wm/mannaggia/mannaggia0911/mannaggia091100006/5957156-jack-of-hearts-playing-card.jpg?ver=6'
+Card.create name: 'Ace', image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Playing_card_heart_A.svg/400px-Playing_card_heart_A.svg.png'
