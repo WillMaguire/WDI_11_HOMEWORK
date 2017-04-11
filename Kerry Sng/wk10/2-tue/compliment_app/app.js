fa0bad35af814491d9e5b318361ed67a340d976c
Kerry <kerrysng@gmail.com>
2017-04-11 16:47:16 +1000

    compliment_app

---
+++ Kerry Sng/wk10/2-tue/compliment_app/app.js
@@ -0,0 +1,40 @@
+
+var express = require('express')
+var _ = require('underscore')
+var app = express();
+var path = require('path');
+var request = require('request')
+var engine = require('ejs-mate')
+
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static( path.join(__dirname, 'public')))
+
+
+app.engine('ejs', engine);
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res) {
+  var compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Is it Ruby Tuesday yet?",
+  "It's almost beer o'clock",
+  "The Force is strong with you"
+  ];
+
+  var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+  res.render('home', {
+    compliments: _.sample(compliments),
+    color: _.sample(colors)
+  });
+
+});
+
+
+
+
+
+
+
+module.exports = app;
