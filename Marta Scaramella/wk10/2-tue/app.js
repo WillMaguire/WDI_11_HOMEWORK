2d28bb2489c6f248ab8a07f3c5feb1a56c81d90b
Marta Scaramella <marta.scaramella@gmail.com>
2017-04-12 09:00:46 +1000

    homeword plus extension

--- Marta Scaramella/wk10/2-tue/app.js
+++ Marta Scaramella/wk10/2-tue/app.js
@@ -39,7 +39,34 @@ function home(req,res){
   });
 };
 
+function homeName(req,res){
 
+  var compliments = {
+    1: "Your instructors love you",
+    2: "High five = ^5",
+    3: "Is it Ruby Tuesday yet?",
+    4: "It's almost beer o'clock",
+    5: "The Force is strong with you"
+  }
+
+  var colors = {
+    1: "#FFBF00",
+    2: "#0080FF",
+    3: "#01DF3A",
+    4: "#FF0080"
+  }
+
+  var i = Math.floor((Math.random() * 4) + 1);
+  var randomColor = colors[i];
+
+  var name = req.params.name;
+
+  res.render('home',{
+    data: compliments,
+    color: randomColor,
+    name: name
+  });
+};
 app.get('/', home);
-
+app.get('/:name', homeName);
 module.exports = app;
