9d814713baa9380326f16c449041eb0e326b9124
Julian Chan <julian.chan@mail.com>
2017-04-12 23:33:43 +1000

    added level 4

--- Julian Chan/wk10/3-wed/app.js
+++ Julian Chan/wk10/3-wed/app.js
@@ -3,16 +3,18 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var engine = require('ejs-mate');
 var _ = require('./helpers');
+var pgp = require('pg-promise')();
 
 var app = express();
+var db = pgp({ database: 'wdi_compliments' });
 
-var compliments = [
-  "Your instructors love you",
-  "High five = ^5",
-  "Is it Ruby Tuesday yet?",
-  "It's almost beer o'clock",
-  "The Force is strong with you"
-];
+// var compliments = [
+//   "Your instructors love you",
+//   "High five = ^5",
+//   "Is it Ruby Tuesday yet?",
+//   "It's almost beer o'clock",
+//   "The Force is strong with you"
+// ];
 
 var colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"];
 
@@ -23,16 +25,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/:name?', function(req, res) {
-  res.render('index', {
-    compliment: _.sample(compliments),
-    bgColor: _.sample(colors),
-    name: req.params.name || ""
-  });
+  db.one('SELECT $1~ FROM compliments OFFSET floor(random() * (SELECT count(*) FROM compliments)) LIMIT 1', 'body')
+      .then(data => {
+        res.render('index', {
+          compliment: data.body,
+          bgColor: _.sample(colors),
+          name: req.params.name || ""
+        });
+      })
+      .catch(error => {
+        console.log('ERROR:', error);
+        res.status(500).send('Internal server error');
+      });
 });
 
 app.post('/compliments', function(req, res) {
-  compliments.push(req.body.body);
-  res.redirect('back');
+  db.none({
+    name: 'insert-compliment',
+    text: 'INSERT INTO compliments (body) VALUES ($1)',
+    values: req.body.body
+  })
+      .then(() => res.redirect('back'))
+      .catch(error => {
+        console.log('ERROR:', error);
+        res.status(500).send('Internal server error');
+      });
 });
 
 module.exports = app;
---
+++ Julian Chan/wk10/3-wed/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS compliments;
+
+CREATE TABLE compliments (
+  id SERIAL4 PRIMARY KEY,
+  body TEXT
+);
